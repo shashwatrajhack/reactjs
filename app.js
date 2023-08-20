f2e2e7803b920d1fcccda861b7bc0447fb6a215c
shashwatrajhack <er.shashwatraj@gmail.com>
2023-08-20 13:57:48 +0530

    Namaste React

---
+++ app.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1",{
+    id:"heading",xyz:'abs'
+},"Hello World from react!");
+
+console.log(heading);
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(heading);
